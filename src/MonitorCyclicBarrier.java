caf135e6ea9d113dbbc2c2332b8fc059588f9c03
joshuadesmith <joshuadesmith@utexas.edu>
2017-02-07 22:00:44 -0600

    Working on Q1-B

--- src/MonitorCyclicBarrier.java
+++ src/MonitorCyclicBarrier.java
@@ -4,32 +4,20 @@
 public class MonitorCyclicBarrier {
 
     private int parties;
-    private int currentCount;
+    private int count;
 
-    private Object monitor;
+    private final Object indexMonitor;
 
     public MonitorCyclicBarrier(int parties) {
         this.parties = parties;
-        this.currentCount = parties;
-        this.monitor = new Object();
+        this.count = parties;
+        this.indexMonitor = new Object();
     }
 
-    public synchronized int await() throws InterruptedException {
-        synchronized (monitor) {
-            int index = --currentCount;
-            if (index > 0) {
-                while (index > 0) {
-                    monitor.wait();
-                }
-            } else {
-                reset();
-            }
-            return index;
+    public int await() throws InterruptedException {
+        synchronized (indexMonitor) {
+
         }
-    }
-
-    public void reset() {
-        currentCount = parties;
-        monitor.notifyAll();
+        return 0;
     }
 }
