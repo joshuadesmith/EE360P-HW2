944d6d49b1235644ae430dcb8c584c02edd841e8
joshuadesmith <joshuadesmith@utexas.edu>
2017-03-20 17:48:36 -0500

    removed UDP stuff from my client impl; added Davids client impl

--- src/hw4/Client.java
+++ src/hw4/Client.java
@@ -11,49 +11,35 @@ import java.util.Scanner;
  */
 public class Client {
 
-    private static final int TEMP_TCP_PORT = 2018;
-    private static final int TEMP_UDP_PORT = 4036;
     private static final int DATA_BUFFER_SIZE = 512;
-    private static final String TEMP_HOST_NAME = "localhost";
 
     private Socket tcpSocket;
     private DataOutputStream outToServer;
     private DataInputStream inFromServer;
     private int tcpPortNum;
 
-    private DatagramSocket udpSocket;
-    private InetAddress address;
-    private int udpPortNum;
-    
     private String hostAddr;
 
     public static void main (String[] args) {
 
         String hostAddress;
         int tcpPort;
-        int udpPort;
-        int protocol = 1; //1 = TCP, 0 = UDP
+        int protocol = 1; //1 = TCP
 
-        
-        if (args.length != 3) {
-            System.out.println("ERROR: Provide 3 arguments");
-            System.out.println("\t(1) <hostAddress>: the address of the server");
-            System.out.println("\t(2) <tcpPort>: the port number for TCP connection");
-            System.out.println("\t(3) <udpPort>: the port number for UDP connection");
-            System.exit(-1);
+        Scanner sc = new Scanner(System.in);
+        int numServer = sc.nextInt();
+
+        for (int i = 0; i < numServer; i++) {
+            // TODO: parse inputs to get the ips and ports of servers
         }
 
         hostAddress = args[0];
         tcpPort = Integer.parseInt(args[1]);
-        udpPort = Integer.parseInt(args[2]);
-        
 
         Client client = new Client();
         client.tcpPortNum = tcpPort;
-        client.udpPortNum = udpPort;
         client.hostAddr = hostAddress;
 
-        Scanner sc = new Scanner(System.in);
         while(sc.hasNextLine()) {
             String cmd = sc.nextLine();
             String[] tokens = cmd.split(" ");
@@ -102,28 +88,8 @@ public class Client {
         String response = null;
 
         try {
-
-            // UDP protocol
-            if (protocol == 0) {
-                DatagramPacket sendPacket = null;
-                DatagramPacket recPacket = null;
-                byte[] recBuf = new byte[DATA_BUFFER_SIZE];
-                byte[] sendBuf;
-
-                setUpUDPSocket();
-
-                sendBuf = command.getBytes();
-                sendPacket = new DatagramPacket(sendBuf, sendBuf.length, address, udpPortNum);
-                udpSocket.send(sendPacket);
-
-                recPacket = new DatagramPacket(recBuf, recBuf.length);
-                udpSocket.receive(recPacket);
-
-                response = new String(recPacket.getData(), 0, recPacket.getLength());
-            }
-
             // TCP protocol
-            else if (protocol == 1) {
+            if (protocol == 1) {
                 setUpTCPSocket();
                 outToServer.writeUTF(command);
                 System.out.println("Issued Command: " + command);
@@ -147,17 +113,6 @@ public class Client {
             inFromServer = new DataInputStream(tcpSocket.getInputStream());
         } catch (IOException e) {
             System.err.println("IOException in Client.setUpTCPSocket: " + e);
-        }
-    }
-
-    private void setUpUDPSocket() {
-        try {
-            udpSocket = new DatagramSocket();
-            address = InetAddress.getByName(hostAddr);
-        } catch (SocketException e) {
-            System.err.println("SocketException in Client.setUpUDPSocket: " + e);
-        } catch (IOException e) {
-            System.err.println("IOException in Client.setUpUDPSocket: " + e);
         }
     }
 }
---
+++ src/hw4/ClientDavid.java
@@ -0,0 +1,104 @@
+package hw4;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class ClientDavid {
+
+    private Socket tcpSocket;
+    private DataOutputStream outToServer;
+    private DataInputStream inFromServer;
+
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numServer = sc.nextInt();
+        InetSocketAddress[] servers = new InetSocketAddress[numServer];
+        ClientDavid client = new ClientDavid();
+
+        for (int i = 0; i < numServer; i++) {
+            // TODO: parse inputs to get the ips and ports of servers
+            String ip_port[] = sc.nextLine().trim().split(":");
+            String ip = ip_port[0];
+            int port = Integer.parseInt(ip_port[1]);
+            servers[i] = new InetSocketAddress(ip, port);
+        }
+
+        while(sc.hasNextLine()) {
+            String cmd = sc.nextLine();
+            String[] tokens = cmd.split(" ");
+            String response = null;
+
+            if (tokens[0].equals("purchase")) {
+                String command = tokens[0] + " " + tokens[1] + " " + tokens[2] + " " + tokens[3];
+                response = client.issueCommand(command,servers,numServer);
+                System.out.println("Response received: \n" + response);
+            }
+
+            else if (tokens[0].equals("cancel")) {
+                String command = tokens[0] + " " + tokens[1];
+                response = client.issueCommand(command,servers,numServer);
+                System.out.println("Response received: \n" + response);
+            }
+
+            else if (tokens[0].equals("search")) {
+                String command = tokens[0] + " " + tokens[1];
+                response = client.issueCommand(command,servers,numServer);
+                System.out.println("Response received: \n" + response);
+            }
+
+            else if (tokens[0].equals("list")) {
+                response = client.issueCommand(tokens[0],servers,numServer);
+                System.out.println("Response received: \n" + response);
+            }
+            else {
+                System.out.println("ERROR: No such command");
+            }
+        }
+    }
+
+    private String issueCommand(String command, InetSocketAddress[] servers,int numServer) {
+        String response = null;
+
+        try {
+            setUpTCPSocket(servers,numServer);
+            outToServer.writeUTF(command);
+            System.out.println("Issued Command: " + command);
+            outToServer.flush();
+
+            response = inFromServer.readUTF();
+            tcpSocket.close();
+
+
+        } catch (IOException e) {
+            System.err.println("IOException in Client.issueCommand: " + e);
+        }
+
+        return response;
+    }
+
+    private void setUpTCPSocket(InetSocketAddress[] servers,int numServer) {
+        boolean server_found = false;
+        InetSocketAddress currentServer;
+        int index = 0;
+        while(!server_found){
+            tcpSocket = new Socket();
+            currentServer = servers[index];
+            try {
+					/* handle TCP connection to server */
+                tcpSocket.setSoTimeout(100);
+                try {
+                    tcpSocket.connect(currentServer);
+                } catch (Exception e) {
+                    index = (index + 1) % numServer;
+                    continue;
+                }
+                outToServer = new DataOutputStream(tcpSocket.getOutputStream());
+                inFromServer = new DataInputStream(tcpSocket.getInputStream());
+                server_found = true;
+            }catch(IOException e){
+                System.err.println("IOException in Client.setUpTCPSocket finding server: " + e);
+            }
+        }
+    }
+}
