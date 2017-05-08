9aaf0d13a70632c2130019445be4b5beb2812790
joshuadesmith <joshuadesmith@utexas.edu>
2017-05-08 14:40:26 -0500

    added solution to Multiroom problem.. used reentrant lock and condition

---
+++ src/ch3problems/Multiroom.java
@@ -0,0 +1,54 @@
+package ch3problems;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by joshuasmith on 5/8/17.
+ */
+public class Multiroom {
+    ReentrantLock lock = new ReentrantLock();
+    Condition onlyOneRoom = lock.newCondition();
+
+    boolean[] occupied;
+    int numOccupants = 0;
+
+    public Multiroom(int numRooms) {
+        this.occupied = new boolean[numRooms];
+        for (boolean b : this.occupied) b = false;
+    }
+
+    public void joinRoom(int room) {
+        if (room < 0 || room >= occupied.length) System.out.println("Invalid room number: " + room);
+
+        else {
+            try {
+                while (!canJoin(room)) { onlyOneRoom.await(); }
+                if (!occupied[room]) occupied[room] = true;
+                numOccupants++;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void leaveRoom(int room) {
+        if (room < 0 || room >= occupied.length) System.out.println("Invalid room number: " + room);
+        if (numOccupants == 0) System.out.println("Can't leave an empty room");
+        else {
+            if (--numOccupants == 0) {
+                occupied[room] = false;
+                onlyOneRoom.signalAll();
+            }
+        }
+    }
+
+    private boolean canJoin(int room) {
+        for (int i = 0; i < occupied.length; i++) {
+            if (i != room) {
+                if (occupied[i]) return false;
+            } else if (occupied[i]) return true;
+        }
+        return false;
+    }
+}
--- src/ch3problems/SleepingCounter.java
+++
@@ -1,7 +0,0 @@
-package ch3problems;
-
-/**
- * Created by joshuasmith on 5/8/17.
- */
-public class SleepingCounter {
-}
